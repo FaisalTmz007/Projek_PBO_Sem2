7c17707c13d7ebb1ac758306a258f66050b52dcd
FaisalTmz007 <faisaltemas007@gmail.com>
2022-06-17 14:02:56 +0700

    kurang info pasien dan kamar

---
+++ Home.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBO_ManajemenRumahSakit
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void patientRegistrationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Regis obj1 = new Regis();
+            obj1.ShowDialog();
+        }
+
+        private void patientInformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dokter obj2 = new Dokter();
+            obj2.ShowDialog();
+        }
+
+        private void roomInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Kamar obj1 = new Kamar();
+            obj1.ShowDialog();
+        }
+
+        private void addStaffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void closeApp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
---
+++ Kamar.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace PBO_ManajemenRumahSakit
+{
+    partial class Kamar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.keywordTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(54, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informasi Kamar";
+            // 
+            // keywordTextBox
+            // 
+            this.keywordTextBox.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.keywordTextBox.Location = new System.Drawing.Point(30, 104);
+            this.keywordTextBox.Name = "keywordTextBox";
+            this.keywordTextBox.Size = new System.Drawing.Size(217, 30);
+            this.keywordTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Segoe UI", 10.2F);
+            this.searchButton.Location = new System.Drawing.Point(268, 101);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(108, 34);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Cari";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 160);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(344, 278);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Kamar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(412, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.keywordTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Kamar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kamar";
+            this.Load += new System.EventHandler(this.Kamar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox keywordTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
